ffdac6543e709feae4e3236073f432357726f4bc
이기민 <oscar351@naver.com>
2022-02-22 15:42:03 +0900

    save

--- app.js
+++ app.js
@@ -3,9 +3,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var cors = require('cors');
+var dotenv = require('dotenv');
+var bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const router = require('./routes/index');
+
 
 var app = express();
 
@@ -16,6 +22,7 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('login', { title: 'Express' });
+});
+
+router.post('/findUser', function(req, res, next) {
+  res.render('findUser');
+});
+
+router.post('/regist', function(req, res, next) {
+  res.render('regist');
 });
 
 module.exports = router;
