06a8bc3372fb873d5fc0ff496da179ae8baadd51
이기민 <oscar351@naver.com>
2022-03-04 17:45:07 +0900

    save

--- app.js
+++ app.js
@@ -10,8 +10,6 @@ var crypto = require("crypto");
 var FileStore = require('session-file-store')(session);
 var dotenv = require('dotenv');
 var bodyParser = require('body-parser');
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -55,22 +53,6 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
-});
-
-io.on('connection', (socket) => {   //연결이 들어오면 실행되는 이벤트
-  // socket 변수에는 실행 시점에 연결한 상대와 연결된 소켓의 객체가 들어있다.
-  
-  //socket.emit으로 현재 연결한 상대에게 신호를 보낼 수 있다.
-  socket.emit('usercount', io.engine.clientsCount);
-
-  // on 함수로 이벤트를 정의해 신호를 수신할 수 있다.
-  socket.on('message', (msg) => {
-      //msg에는 클라이언트에서 전송한 매개변수가 들어온다. 이러한 매개변수의 수에는 제한이 없다.
-      console.log('Message received: ' + msg);
-
-      // io.emit으로 연결된 모든 소켓들에 신호를 보낼 수 있다.
-      io.emit('message', msg);
-  });
 });
 
 var server=app.listen(8888, function(){
